6bf78e9cdcb437d9c534e8f189e2ee50748986b0
ShenMian <sms_school@outlook.com>
2025-04-22 05:30:56 +0100

    chore: adjust main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,4 +2,4 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Stone.get_node("RigidBody3D").apply_force(Vector3(-650.0, 0.0, -650.0))
+	$Stone.get_node("RigidBody3D").apply_force(Vector3(0.0, 0.0, -900.0))

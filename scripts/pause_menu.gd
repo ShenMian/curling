6384c4e6e620511b4f9fe6fba9540bdef8867501
ShenMian <sms_school@outlook.com>
2025-04-24 21:55:07 +0100

    fix: fix pause menu also paused main menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -16,4 +16,5 @@ func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 
 func _on_quit_button_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main.tscn")

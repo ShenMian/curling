2b06441fb7faa856238e3e974651511b7995cea6
ShenMian <sms_school@outlook.com>
2025-04-25 08:21:47 +0100

    refactor(sheet): remove unused _ready and _process functions

--- scripts/sheet.gd
+++ scripts/sheet.gd
@@ -2,14 +2,8 @@ extends Node3D
 
 signal out_of_bounds(node: Node3D)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func is_body_in_house(body: Node3D) -> bool:
+	return $HouseArea.overlaps_body(body)
 
 func _on_body_exited(body: Node3D) -> void:
 	out_of_bounds.emit(body)

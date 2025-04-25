b01827bf9ff9f51cebd0704ce9a4d2bc0805ad99
ShenMian <sms_school@outlook.com>
2025-04-25 03:49:15 +0100

    feat(gameplay): adjust camera offset and look-at logic for improved view

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -5,9 +5,9 @@ var stone_scene: PackedScene = preload("res://scenes/stone.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var stone = stone_scene.instantiate()
-	stone.color = Color.BLUE
 	stone.position = Vector3(0.0, 0.2, 0.3)
 	stone.rotation_degrees = Vector3(0.0, 180.0, 0.0)
+	stone.color = Color.BLUE
 	$Stones.add_child(stone)
 	$ThirdPersonCamera.target = stone
 
@@ -22,4 +22,3 @@ func _input(_event):
 
 func _on_sheet_out_of_bounds(_stone: Node3D) -> void:
 	print("stone out of bounds")
-	pass # Replace with function body.

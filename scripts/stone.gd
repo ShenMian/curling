44977aa8dff8e1518e773e284f84363136337b36
ShenMian <sms_school@outlook.com>
2025-04-26 07:29:35 +0100

    feat: add NumberLabel node and export number variable for stone

--- scripts/stone.gd
+++ scripts/stone.gd
@@ -1,6 +1,9 @@
 extends RigidBody3D
 
 @export var color: Color
+@export var number: int
+
+@onready var number_label: Label3D = $NumberLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,3 +11,4 @@ func _ready() -> void:
 	material.albedo_color = color
 	material.roughness = 0.2
 	$Meshes/Body.set_material_override(material)
+	number_label.text = str(number)

13dcea0b932ec328b68090cc212b4b3f45250684
ShenMian <sms_school@outlook.com>
2025-05-02 01:16:58 +0100

    feat(pause_menu): add retry button functionality and update translations

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -11,6 +11,10 @@ func _on_resume_button_pressed() -> void:
 	hide()
 	get_tree().paused = false
 
+func _on_retry_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
 func _on_quit_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main.tscn")

0cab04b07c68c8df432a1b86bcd5674d3a651e4a
ShenMian <sms_school@outlook.com>
2025-05-04 05:46:21 +0100

    fix(stone): update comment for clarity in _ready function

--- scripts/stone.gd
+++ scripts/stone.gd
@@ -6,13 +6,14 @@ class_name Stone
 
 @onready var number_label: Label3D = $NumberLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Ensure each stone has an independent StandardMaterial3D
 	var material := StandardMaterial3D.new()
 	material.albedo_color = color
 	material.roughness = 0.2
 	$Meshes/Body.set_material_override(material)
 	number_label.text = str(number)
+
 
 # Plays the impact sound when the stone collides with another stone.
 func _on_body_entered(body: Node) -> void:
